2555963affb2d2e30e99725eee856e2ffe7202ce
Surwxa5 <nepalisurakshya767@gmail.com>
2026-04-25 19:00:39 +0545

    json file

--- jsPracticeQuestion/fetchData.js
+++ jsPracticeQuestion/fetchData.js
@@ -3,6 +3,7 @@ async function loadUsers (){
   const response = await fetch("https://jsonplaceholder.typicode.com/users")
   // we have to wait until the data fetch and goes to response
   const data = await response.json();
+  // All the data will be printed stored in the data in key value pairs 
   console.log(data)
   data.forEach(users => {
     console.log(users.name)
