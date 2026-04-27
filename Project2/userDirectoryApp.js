fd928d5bc03ea91fd4a1c7d5272dbcb0d1688811
Surwxa5 <nepalisurakshya767@gmail.com>
2026-04-27 17:21:45 +0545

    fetching data

--- Project2/userDirectoryApp.js
+++ Project2/userDirectoryApp.js
@@ -1,3 +1,14 @@
-async function name(params) {
-  
+const card = document.getElementById("card1")
+
+async function userDirectory() {
+  try{
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await response.json()
+    console.log(data)
+    card.innerHTML +=data 
+
+  } catch(error){
+    console.log("something wrong !")
+  }
 }
+userDirectory()
