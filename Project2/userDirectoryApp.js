79baa90962b5d590339ea47dae5e655d7fd733ee
Surwxa5 <nepalisurakshya767@gmail.com>
2026-04-28 20:05:57 +0545

    user directory data fetching

--- Project2/userDirectoryApp.js
+++ Project2/userDirectoryApp.js
@@ -1,14 +1,34 @@
-const card = document.getElementById("card1")
-
+// store all the users here so the search can access
+ let allUsers = [];
 async function userDirectory() {
   try{
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
     const data = await response.json()
-    console.log(data)
-    card.innerHTML +=data 
+    allUsers = data
+    document.getElementById("loading").style.display = "none"
+    showCards(allUsers)
 
   } catch(error){
-    console.log("something wrong !")
+    document.getElementById("loading").innerText = "Failed"
   }
 }
+
+function showCards(users){
+  const container = document.getElementById("cards-container")
+  let allCards = "";
+  users.forEach(user => {
+    allCards += `<div class = "card">
+    <h2>${user.name}</h2>
+    </div>
+    `
+  });
+  container.innerHTML = allCards;
+}
+
+document.getElementById("search").addEventListener("input",function(){
+  const typed = this.value.toLowerCase()
+  // filter users where name includes what you typed 
+  const filtered = users.filter(user => user.name.toLowerCase().includes(type))
+  showCards(filtered)
+})
 userDirectory()
